a84134583a05afd2fcd2e517441d58c6d524be70
linear-digital <tamizuddin.tr@gmail.com>
2024-09-05 01:08:05 +0600

    update Newsletter.tsx

--- Components/Pages/Home/Newsletter.tsx
+++ Components/Pages/Home/Newsletter.tsx
@@ -11,6 +11,12 @@ const Newsletter = () => {
                 <p className='max-w-[505px] mt-2 text-[#898989]'>
                 Join thousands of savvy shoppers who trust us for the best deals and product insights. No spam, we promise!
                 </p>
+                <div className="flex w-[386px] items-center mt-10 h-[55px]">
+                    <input type="text" placeholder='Enter your email' className='w-full h-full border border-none rounded-l-lg px-4 py-2 outline-none' />
+                    <button className='bg-primary text-white rounded-r-lg px-4 py-2 h-full'>
+                        Subscribe
+                    </button>
+                </div>
             </div>
             <Image src={'/images/chair.png'} width={488} height={535} alt="chair" className='absolute bottom-0 right-10' />
         </div>
