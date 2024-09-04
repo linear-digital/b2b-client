8c74decf36149be18099ed237c0032b1c4a6e8ce
linear-digital <tamizuddin.tr@gmail.com>
2024-09-04 22:44:47 +0600

    Completed Shop by Category

--- Components/Pages/Home/ShopCategory.tsx
+++ Components/Pages/Home/ShopCategory.tsx
@@ -1,7 +1,17 @@
+'use client'
 import Link from 'next/link';
 import React from 'react';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+import Image from 'next/image';
+
+
 const ShopCategory = () => {
     return (
         <div className='container '>
@@ -35,6 +45,42 @@ const ShopCategory = () => {
                     </button>
                 </div>
             </div>
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={30}
+                className="mt-10 h-[300px]"
+            >
+                <SwiperSlide className='relative'>
+                    <Image src={'/photos/image1.png'} alt="login" width={300} height={300} className={'h-full w-full object-cover'} />
+                    <h4 className='absolute top-4 left-4 text-white px-1 bg-primary'>
+                        Product Category
+                    </h4>
+                </SwiperSlide>
+                <SwiperSlide className='relative'>
+                    <Image src={'/products/product-1.png'} alt="login" width={300} height={300} className={'h-full w-full object-cover'} />
+                    <h4 className='absolute top-4 left-4 text-white px-1 bg-primary'>
+                        Product Category
+                    </h4>
+                </SwiperSlide>
+                <SwiperSlide className='relative'>
+                    <Image src={'/photos/image3.png'} alt="login" width={300} height={300} className={'h-full w-full object-cover'} />
+                    <h4 className='absolute top-4 left-4 text-white px-1 bg-primary'>
+                        Product Category
+                    </h4>
+                </SwiperSlide>
+                <SwiperSlide className='relative'>
+                    <Image src={'/photos/image4.png'} alt="login" width={300} height={300} className={'h-full w-full object-cover'} />
+                    <h4 className='absolute top-4 left-4 text-white px-1 bg-primary'>
+                        Product Category
+                    </h4>
+                </SwiperSlide>
+                <SwiperSlide className='relative'>
+                    <Image src={'/photos/image5.png'} alt="login" width={300} height={300} className={'h-full w-full object-cover'} />
+                    <h4 className='absolute top-4 left-4 text-white px-1 bg-primary'>
+                        Product Category
+                    </h4>
+                </SwiperSlide>
+            </Swiper>
         </div>
     );
 };
