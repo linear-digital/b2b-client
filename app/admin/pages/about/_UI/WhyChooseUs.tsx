a1cab9ae0ea79e35e43d3f98a46e84620551b76b
linear-digital <tamizuddin.tr@gmail.com>
2024-09-16 21:17:13 +0600

    update page.tsx and WhyShopWithUs.tsx

---
+++ app/admin/pages/about/_UI/WhyChooseUs.tsx
@@ -0,0 +1,143 @@
+
+'use client';
+import fetcher from '@/Components/util/axios';
+import { errorDisplay } from '@/Components/util/readError';
+import { useQuery } from '@tanstack/react-query';
+import { Button, Form, Input, Popover, Spin } from 'antd';
+import Image from 'next/image';
+import React, { useEffect } from 'react';
+import toast from 'react-hot-toast';
+
+const Why_Choose_Us = () => {
+
+    const { data, isFetching, refetch } = useQuery({
+        queryKey: ['Why_Choose_Us'],
+        queryFn: () => {
+            const res: any = fetcher({
+                url: `/pages/search`,
+                method: 'POST',
+                body: {
+                    name: "Why_Choose_Us"
+                }
+            })
+            return res
+        },
+        refetchOnWindowFocus: false
+    })
+    const onFinish = async (values: any) => {
+        try {
+            const res = await fetcher({
+                method: 'PUT',
+                url: `/pages/${data?._id}`,
+                body: {
+                    ...values,
+                    others: {
+
+                    }
+                }
+            })
+            toast.success("Data updated successfully")
+            refetch()
+        } 
+        catch (error) {
+            toast.error(errorDisplay(error))
+        }
+    }
+    if (isFetching) {
+        return <Spin size='large' />
+    }
+    return ( 
+        <div id='Why-Shop' className='mt-20 border p-4 rounded-lg'>
+            <h1 className='text-3xl font-medium font-elMessiri'>{data?.title}</h1>
+            <Form
+                layout='vertical'
+                initialValues={data}
+                className='mt-4'
+                onFinish={onFinish}
+            >
+                <Form.Item
+                    label="Title"
+                    name={"title"}
+                >
+                    <Input size='large' />
+                </Form.Item>
+                <Form.Item>
+                    <Button type='primary' htmlType='submit'>
+                        Update
+                    </Button>
+                </Form.Item>
+            </Form>
+            <section className='mt-10'>
+                <h1 className='text-2xl font-medium font-elMessiri'>{"Items"}</h1>
+                <div className="mt-10 grid grid-cols-2 gap-10">
+                    {
+                        data?.others?.options?.map((item: any, index: number) => <ItemCard
+                            data={item}
+                            key={index}
+                            index={index}
+                            options={data?.others?.options}
+                            id={data?._id}
+                        />)
+                    }
+                </div>
+
+            </section>
+        </div>
+    );
+};
+
+export default Why_Choose_Us;
+
+const ItemCard = ({ data, index, options, id }: { data: any, index: number, options: any, id?: string }) => {
+    const onFinish = async (values: any) => {
+        const newData = options.map((item: any, i: number) => {
+            if (i === index) {
+                return {
+                    ...item,
+                    ...values
+                }
+            }
+            return item
+        })
+        try {
+            const res = await fetcher({
+                method: 'PUT',
+                url: `/pages/${id}`,
+                body: {
+                    others: {
+                        options: newData
+                    }
+                }
+            })
+            toast.success("Data updated successfully")
+        } catch (error) {
+            toast.error(errorDisplay(error))
+        }
+    }
+    return <div>
+        <Image src={data.image} alt="login" width={80} height={80} />
+        <Form className="mt-4"
+            layout='vertical'
+            initialValues={data}
+            onFinish={onFinish}
+        >
+            <Form.Item
+                label="Title"
+                name="title"
+            >
+                <Input size='large' />
+            </Form.Item>
+            <Form.Item
+                label="Description"
+                name="description"
+            >
+                <Input size='large' />
+            </Form.Item>
+            <Form.Item>
+                <Button type='primary' className='mt-5' size='large' htmlType='submit'>
+                    Update Options
+                </Button>
+            </Form.Item>
+        </Form>
+    </div>
+}
--- app/admin/pages/about/page.tsx
+++ app/admin/pages/about/page.tsx
@@ -5,6 +5,7 @@ import OurMission from './_UI/OurMission';
 import WhatWoDO from './_UI/WhatWeDo';
 import Partnerships from './_UI/Partnerships';
 import Affiliate from './_UI/Affiliate';
+import Why_Choose_Us from './_UI/WhyChooseUs';
 
 const page = () => {
     return (
@@ -14,6 +15,7 @@ const page = () => {
             <WhatWoDO />
             <Partnerships />
             <Affiliate />
+            <Why_Choose_Us />
         </div>
     );
 };
