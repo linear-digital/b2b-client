93f7b72d021e0c4f3c4fe0ff6130d94a9f9bc7d2
linear-digital <tamizuddin.tr@gmail.com>
2024-10-25 21:21:57 +0600

    kfjdflkd

---
+++ app/sitemap.ts
@@ -0,0 +1,64 @@
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: 'https://www.shoppanorma.com/',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 1,
+        },
+        {
+            url: 'https://www.shoppanorma.com/products',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        }, {
+            url: 'https://www.shoppanorma.com/voucher',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        }, {
+            url: 'https://www.shoppanorma.com/about',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        }, {
+            url: 'https://www.shoppanorma.com/contact',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        }, {
+            url: 'https://www.shoppanorma.com/wishlist',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        }, {
+            url: 'https://www.shoppanorma.com/auth/login',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        }, 
+        
+        {
+            url: 'https://www.shoppanorma.com/auth/signup',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+
+        {
+            url: 'https://www.shoppanorma.com/privacy-policy',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://www.shoppanorma.com/terms-conditions',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+
+    ]
+}
