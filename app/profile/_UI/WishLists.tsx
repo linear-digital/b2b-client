fd0f9880291203101ca4d10e4b830b0c0b735c24
linear-digital <tamizuddin.tr@gmail.com>
2024-10-25 16:26:41 +0600

    update WishLists.tsx and WishlistCard.tsx

--- app/profile/_UI/WishLists.tsx
+++ app/profile/_UI/WishLists.tsx
@@ -1,19 +1,44 @@
-import { Card } from 'antd';
+'use client';
+import { Card, Spin } from 'antd';
 import React from 'react';
 import WishListCard from './WishlistCard';
+import { useQuery } from '@tanstack/react-query';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/Redux/store';
+import fetcher from '@/Components/util/axios';
 
 const WishLists = () => {
+    const { currentUser } = useSelector((state: RootState) => state.user)
+    const { data: wishlist, isLoading, refetch } = useQuery({
+        queryKey: ['Wishlist'],
+        queryFn: () => {
+            const res: any = fetcher({
+                url: `/wishlist/user/${currentUser?._id}`,
+                method: 'GET',
+            })
+            return res
+        },
+        enabled: !!currentUser
+    })
+    if (isLoading) {
+        return <Spin size='large' fullscreen/>
+    }
     return (
         <Card className='border-0'>
             <h3 className='messiri text-[28px]'>
                 WishLists
             </h3>
             <div className='flex flex-col gap-4 mt-5'>
-                <WishListCard />
-                <WishListCard />
-                <WishListCard />
-                <WishListCard />
-                 <h1 className='text-center mt-10 text-xl font-medium'>No Wishlists Found</h1>
+                {
+                    wishlist?.length < 1 ?
+                        <h1 className='text-center mt-10 text-xl font-medium'>No Wishlists Found</h1>
+                        :
+
+                        wishlist?.map((item: any) => (
+                            <WishListCard refetch={refetch} key={item?._id} data={item} />
+                        ))
+                }
+
             </div>
         </Card>
     );
--- app/profile/_UI/WishlistCard.tsx
+++ app/profile/_UI/WishlistCard.tsx
@@ -45,38 +45,60 @@ import WishImg from '../../wishlist/image.png';
 import { Card } from 'antd';
 import Image from 'next/image';
 import React from 'react';
+import { ProductType } from '@/Components/util/type';
+import fetcher from '@/Components/util/axios';
+import toast from 'react-hot-toast';
+import { errorDisplay } from '@/Components/util/readError';
+import Link from 'next/link';
 
-const WishListCard = () => {
+const WishListCard = ({ data, refetch }: { data: any, refetch: any }) => {
+    const deleteProduct = async () => {
+        try {
+            const response = await fetcher({
+                url: `/wishlist/${data?._id}`,
+                method: 'DELETE'
+            })
+            refetch()
+            toast.success('Item deleted successfully')
+        } catch (error) {
+            toast.error(errorDisplay(error))
+        }
+    }
     return (
         <Card className='border-0  relative'>
             <div className="flex lg:flex-row flex-col lg:items-start gap-x-5">
-                <Image src={WishImg} alt="login" width={140} height={140} 
-                className='w-full lg:max-w-[140px] h-auto lg:w-auto'
+                <Image src={data?.images[0].url} alt="Product" width={140} height={140}
+                    className='w-full lg:max-w-[140px] h-auto lg:w-auto'
                 />
                 <div className='w-full mt-4 lg:mt-0'>
                     <div className="flex items-center justify-between">
                         <h2 className='messiri text-[18px] font-semibold'>
-                            Apple MacBook Pro 16-inch
+                            {data.title}
                         </h2>
                         <Image
+                            onClick={deleteProduct}
                             src={DeleteImg}
                             width={44}
                             height={44}
                             alt='delete'
-                            className='absolute lg:relative top-[30px] right-[30px]'
+                            className='absolute lg:relative top-[30px] right-[30px] cursor-pointer'
                         />
                     </div>
                     <div className="">
                         <h3 className='text-[28px] font-medium'>
-                            $2,499.00
+                            ${data.price}
                         </h3>
                         <div className="flex items-center gap-x-3 lg:mt-0 mt-4">
-                            <button className='btn-primary active'>
-                                View Deals
-                            </button>
-                            <button className='btn-primary-active'>
-                                Go to store
-                            </button>
+                            <Link href={`/products/${data?.offerId}`}>
+                                <button className='btn-primary active'>
+                                    View Deals
+                                </button>
+                            </Link>
+                            <a href={data.goUrl}>
+                                <button className='btn-primary-active'>
+                                    Go to store
+                                </button>
+                            </a>
                         </div>
                     </div>
                 </div>
