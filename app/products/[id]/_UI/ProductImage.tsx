7cc22628c13dfaaeb6772aa38fcbb4328f64915e
linear-digital <tamizuddin.tr@gmail.com>
2024-10-30 21:44:50 +0600

    update ProductImage.tsx

--- app/products/[id]/_UI/ProductImage.tsx
+++ app/products/[id]/_UI/ProductImage.tsx
@@ -8,14 +8,14 @@ import { Image as AntdImage } from 'antd'
 
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import { LinkOutlined, ShareAltOutlined } from '@ant-design/icons';
+import { ShareAltOutlined } from '@ant-design/icons';
 import toast from 'react-hot-toast';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/Redux/store';
 
 
 const ProductImage = ({ product }: { product: ProductType }) => {
-    const {currentUser} = useSelector((state: RootState) => state.user)
+    const { currentUser } = useSelector((state: RootState) => state.user)
     const [swiper, setSwiper] = useState<any>(null);
     const [width, setWidth] = useState(0);
     useEffect(() => {
@@ -57,13 +57,13 @@ const ProductImage = ({ product }: { product: ProductType }) => {
             </div>
             <div className='lg:min-w-[505px] p-5 w-full lg:w-[505px] lg:h-[550px] rounded-[10px]  flex items-center justify-center relative bg-white '>
                 {
-                    currentUser?.role === "admin" &&  <button className='absolute top-4 right-4 '
-                    onClick={copyToClipboard}
-                >
-                   <ShareAltOutlined  className='text-2xl hover:text-primary'/>
-                </button>
+                    currentUser?.role === "admin" && <button className='absolute top-4 right-4 '
+                        onClick={copyToClipboard}
+                    >
+                        <ShareAltOutlined className='text-2xl hover:text-primary' />
+                    </button>
                 }
-               
+
                 <AntdImage src={selected || product?.images[0].zoomUrl} alt="Product Image" className={" p-3 rounded-lg lg:w-[422px] max-h-[422px] object-contain"} />
                 <button
                     className='absolute top-1/2 left-3 -translate-y-1/2 '
